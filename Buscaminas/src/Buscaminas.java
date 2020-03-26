dc8615379894513a14966d096421d8aa700826f2
jose marcial <jose.reyes@aeris.net>
2020-03-25 22:36:58 -0600

    reducing complexity

--- Buscaminas/src/Buscaminas.java
+++ Buscaminas/src/Buscaminas.java
@@ -7,54 +7,57 @@ public class Buscaminas {
 		
 	public static void main (String arg[]) {
 		
-		int size =4;
+		
+		//reading the values from imput 
+		int size =2;
 		int size2 = 4;	
 		
-		String input = "*...\n....\n.*..\n....";
+		String input = "*...\n....";
 		
 		String [] arrays =input.split("\n");
 			
-		
+		// creating the arrays
 		char [][] array = new char [size][size2];
 		for (int x=0 ; x<arrays.length ; x++ ) {
-			System.out.println(arrays[x] );
+			//System.out.println(arrays[x] );
 			array[x]= arrays[x].toCharArray();
 		}
 		
-		for (char[] arr : array ) {
-		System.out.println(Arrays.toString(arr));
-		}
+		// solution 
 		
-		List<String> bombs = new ArrayList<>();
 		
-		for (int x = 0; x < size; x++) {
-
-			for (int j = 0; j < size2; j++) {
-				if (array[x][j] == '*') {
-					System.out.println("yes");
-					bombs.add(String.valueOf(x)+String.valueOf(j));
-				}
-			}
-		}
+//		for (char[] arr : array ) {
+//		System.out.println(Arrays.toString(arr));
+//		}
+//		
+//		List<String> bombs = new ArrayList<>();
 		
 		for (int x = 0; x < size; x++) {
 
 			for (int j = 0; j < size2; j++) {
+//				if (array[x][j] == '*') {
+//					System.out.println("yes");
+//					//bombs.add(String.valueOf(x)+String.valueOf(j));
+//				}
+				
 				if (array[x][j] == '.') {
-					System.out.println("yes");
+					//System.out.println("yes");
 					List<String> sPoints = generateSP( x , j);
 					int counter = 48;
 					for (String sp  : sPoints ) {
-						if (bombs.contains(sp)) {
+						if (isvalid(sp,size, size2) && array[Character.getNumericValue(sp.charAt(0))][Character.getNumericValue(sp.charAt(1))]=='*') {
 						counter++;
 						}
 					}
-					System.out.println(counter);
+					//System.out.println(counter);
 					array[x][j]= (char) counter;
 				}
+				
+				
 			}
 		}
 		
+
 		
 		for (char[] arr : array ) {
 			System.out.println(Arrays.toString(arr));
@@ -65,16 +68,33 @@ public class Buscaminas {
 		
 	}
 
+	private static boolean isvalid(String sp, int size, int size2) {
+		if	(sp.contains("-")) {
+			return false;
+		}
+		if (Character.getNumericValue(sp.charAt(0)) >= size){
+			return false;
+		}
+		if (Character.getNumericValue(sp.charAt(1)) >= size2){
+			return false;
+		}
+		
+		
+		return true;
+	}
+
+	//Generate a list of surrounding points
 	private static List<String> generateSP(int x, int y) {
+		
 		List<String> sp = new ArrayList<>();
-		sp.add(new String (String.valueOf(x-1)+String.valueOf(y-1)));
-		sp.add(new String (String.valueOf(x-1)+String.valueOf(y)));
-		sp.add(new String (String.valueOf(x-1)+String.valueOf(y+1)));
-		sp.add(new String (String.valueOf(x)+String.valueOf(y-1)));
-		sp.add(new String (String.valueOf(x)+String.valueOf(y+1)));
-		sp.add(new String (String.valueOf(x+1)+String.valueOf(y-1)));
-		sp.add(new String (String.valueOf(x+1)+String.valueOf(y)));
-		sp.add(new String (String.valueOf(x+1)+String.valueOf(y+1)));
+		sp.add(String.valueOf(x-1)+String.valueOf(y-1));
+		sp.add(String.valueOf(x-1)+String.valueOf(y));
+		sp.add(String.valueOf(x-1)+String.valueOf(y+1));
+		sp.add(String.valueOf(x)+String.valueOf(y-1));
+		sp.add(String.valueOf(x)+String.valueOf(y+1));
+		sp.add(String.valueOf(x+1)+String.valueOf(y-1));
+		sp.add(String.valueOf(x+1)+String.valueOf(y));
+		sp.add(String.valueOf(x+1)+String.valueOf(y+1));
 		
 		return sp;
 		
